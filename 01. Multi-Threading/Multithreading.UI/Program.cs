3604cf1d834f8e3b111c0fbf8161ef8edb5d7ffa
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2018-03-18 23:30:28 +0300

    task 1: add initial code

---
+++ 01. Multi-Threading/Multithreading.UI/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Multithreading.UI
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //TaskIterations();
+            //TaskChain();
+            //MultiplyMatrices();
+            RecursiveThreads();
+            //RecursiveThreadPool();
+            //SharedCollection();
+        }
+
+        private static void TaskIterations()
+        {
+            var taskCount = 100;
+            var tasks = new Task[taskCount];
+            var taskBuilder = new IterationTaskBuilder();
+
+            for (int i = 0; i < taskCount; i++)
+            {
+                var task = taskBuilder.Build(Console.WriteLine);
+                task.Start();
+                tasks[i] = task;
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        private static void TaskChain()
+        {
+            var taskBuilder = new ChainTaskService();
+            
+            var taskChain = taskBuilder.CreateRandomArray(Console.WriteLine)
+                .ContinueWith((x) => taskBuilder.AddRandomNumber(Console.WriteLine, x.Result))
+                .Unwrap()
+                .ContinueWith(x => taskBuilder.SortArray(Console.WriteLine, x.Result))
+                .Unwrap()
+                .ContinueWith(x => taskBuilder.CalculateTheAverageValue(Console.WriteLine, x.Result))
+                .Unwrap();
+
+            Task.WaitAll(taskChain);
+        }
+
+        private static void MultiplyMatrices()
+        {
+            var firstMatrix = new[,]
+            {
+                { 5, 6, 3, 7 },
+                { 4, 7, 9, 10 },
+                { 4, 7, 13, 2 },
+            };
+
+            var secondMatrix = new[,]
+            {
+                { 5, 6, 3, 7, 8 },
+                { 4, 7, 9, 10, 6 },
+                { 4, 5, 13, 2, 1 },
+                { 18, 2, 9, 14, 0 },
+            };
+            
+            var matrixService = new MatrixService();
+            var result = matrixService.Multiply(firstMatrix, secondMatrix);
+
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write($"{result[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void RecursiveThreads()
+        {
+            var threadService = new ThreadService();
+            threadService.UseThreads(Console.WriteLine);
+        }
+
+        private static void RecursiveThreadPool()
+        {
+            var threadService = new ThreadService();
+            threadService.UseThreadPool(Console.WriteLine);
+        }
+
+        private static void SharedCollection()
+        {
+            var threadService = new ThreadService();
+            threadService.CreateCollection(Console.WriteLine);
+        }
+    }
+}
---
+++ 01. Multi-Threading/Multithreading/ChainTaskService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class ChainTaskService
+    {
+        private readonly Random _random;
+
+        public ChainTaskService()
+        {
+            _random = new Random();
+        }
+
+        public Task<int[]> CreateRandomArray(Action<string> resultAction)
+        {
+            return Task.Factory.StartNew(() => Perform(resultAction));
+        }
+
+        public Task<int[]> AddRandomNumber(Action<string> resultAction, int[] currentArray)
+        {
+            return Task.Factory.StartNew(() => Perform(resultAction, currentArray));
+        }
+
+        public Task<int[]> SortArray(Action<string> resultAction, int[] array)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Array.Sort(array);
+                resultAction(string.Join(", ", array));
+                return array;
+            });
+        }
+
+        public Task<double> CalculateTheAverageValue(Action<string> resultAction, int[] array)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var average = (double)array.Sum() / array.Length;
+                resultAction($"{average:#.###}");
+                return average;
+            });
+        }
+
+        private int[] Perform(Action<string> action)
+        {
+            var count = 10;
+            var array = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = _random.Next(100);
+            }
+
+            action(string.Join(", ", array));
+
+            return array;
+        }
+
+        private int[] Perform(Action<string> action, int[] currentArray)
+        {
+            var array = new int[currentArray.Length + 1];
+            currentArray.CopyTo(array, 0);
+
+            array[currentArray.Length] = _random.Next(100);
+
+            action(string.Join(", ", array));
+
+            return array;
+        }
+    }
+}
