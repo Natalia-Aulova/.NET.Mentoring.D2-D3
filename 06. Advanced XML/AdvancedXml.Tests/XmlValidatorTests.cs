a6e9e5096ed37964da18ced2eefe996f7aff641a
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2018-05-08 21:39:43 +0300

    task 6: add a test for invalid xml file

--- 06. Advanced XML/AdvancedXml.Tests/XmlValidatorTests.cs
+++ 06. Advanced XML/AdvancedXml.Tests/XmlValidatorTests.cs
@@ -17,5 +17,17 @@ namespace AdvancedXml.Tests
 
             Assert.AreEqual(true, success);
         }
+
+        [TestMethod]
+        public void Test_Validate_InvalidXml_ShouldReturnFalse()
+        {
+            var validator = new XmlValidator();
+            var success = validator.Validate("books_invalid.xml", (lineNumber, linePosition, message) =>
+            {
+                Console.WriteLine($"[{lineNumber}:{linePosition}] {message}");
+            });
+
+            Assert.AreEqual(false, success);
+        }
     }
 }
