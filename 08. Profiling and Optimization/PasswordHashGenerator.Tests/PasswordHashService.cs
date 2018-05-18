d4e0b84bec0a3c077b18d2b8656d3c6c1e28d4ee
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2018-05-19 02:28:25 +0300

    task 8: attempts to optimize GeneratePasswordHashUsingSalt method

--- 08. Profiling and Optimization/PasswordHashGenerator.Tests/PasswordHashService.cs
+++ 08. Profiling and Optimization/PasswordHashGenerator.Tests/PasswordHashService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace PasswordHashGenerator.Tests
 {
@@ -14,6 +15,50 @@ namespace PasswordHashGenerator.Tests
             Array.Copy(salt, 0, hashBytes, 0, 16);
             Array.Copy(hash, 0, hashBytes, 16, 20);
             var passwordHash = Convert.ToBase64String(hashBytes);
+            return passwordHash;
+        }
+
+        public string GeneratePasswordHashUsingSalt_v2(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+            return passwordHash;
+        }
+
+        public string GeneratePasswordHashUsingSalt_v3(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var passwordBytes = new UTF8Encoding(false).GetBytes(passwordText);
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordBytes, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes, 0, 36, Base64FormattingOptions.None);
+            return passwordHash;
+        }
+
+        public string GeneratePasswordHashUsingSalt_v4(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+            var passwordBytes = new UTF8Encoding(false).GetBytes(passwordText);
+             var pbkdf2 = new Rfc2898DeriveBytes(passwordBytes, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes, 0, 36, Base64FormattingOptions.None);
             return passwordHash;
         }
     }
--- 08. Profiling and Optimization/PasswordHashGenerator.Tests/PasswordHashServiceTests.cs
+++ 08. Profiling and Optimization/PasswordHashGenerator.Tests/PasswordHashServiceTests.cs
@@ -1,18 +1,90 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PasswordHashGenerator.Tests
 {
     [TestClass]
     public class PasswordHashServiceTests
     {
-        [TestMethod]
-        public void Test_GeneratePasswordHashUsingSalt()
-        {
-            var password = "123456";
-            var salt = new byte[] { 12, 23, 34, 45, 56, 67, 78, 89, 90, 01, 12, 23, 34, 45, 56, 67 };
+        private readonly int _iterationCount = 50;
+        private readonly string _password = "123456";
+        private readonly byte[] _salt = { 12, 23, 34, 45, 56, 67, 78, 89, 90, 01, 12, 23, 34, 45, 56, 67 };
 
-            var hashService = new PasswordHashService();
-            var passwordHash = hashService.GeneratePasswordHashUsingSalt(password, salt);
+        private PasswordHashService _hashService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _hashService = new PasswordHashService();
+        }
+
+        [TestMethod]
+        public void Test_GeneratePasswordHashUsingSalt_Original()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < _iterationCount; i++)
+            {
+                _hashService.GeneratePasswordHashUsingSalt(_password, _salt);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"original: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        [TestMethod]
+        public void Test_GeneratePasswordHashUsingSalt_v2()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < _iterationCount; i++)
+            {
+                _hashService.GeneratePasswordHashUsingSalt_v2(_password, _salt);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"v2: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        [TestMethod]
+        public void Test_GeneratePasswordHashUsingSalt_v3()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < _iterationCount; i++)
+            {
+                _hashService.GeneratePasswordHashUsingSalt_v3(_password, _salt);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"v3: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        [TestMethod]
+        public void Test_GeneratePasswordHashUsingSalt_v4()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < _iterationCount; i++)
+            {
+                _hashService.GeneratePasswordHashUsingSalt_v4(_password, _salt);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"v4: {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
